b23747f8d228ba6722eb155c1a0fe45867b62c71
BOUAJLA <bouajla.h@hotmail.fr>
2019-12-10 15:51:42 +0100

    update package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
