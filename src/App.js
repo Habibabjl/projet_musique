2344f5c891740685eb2b27dd0e40b4fa8d69bef6
BOUAJLA <bouajla.h@hotmail.fr>
2019-12-30 17:32:07 +0100

    toolbar simple

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Paper from "@material-ui/core/Paper";
 import { Chart } from "react-google-charts";
 import * as artistApi from "./services/ArtistApi";
 
+//import { SearchBar } from 'react-native-elements';
 class App extends React.Component {
 
  constructor(props) {
@@ -31,7 +32,19 @@ class App extends React.Component {
     });
   }
 
+ /* updateSearch = search => {
+    this.setState({ search });
+    
+  };
+   <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />*/
+
+
   render() {
+    const { search } = this.state;
     const artistsWithMostAlbum = this.state.artistsWithMostAlbum.length > 0 && this.state.artistsWithMostAlbum[0].name;
     const datas = this.state.artistsWithMostAlbum.map((el, index) => {
        return [el.name, el.sum];
@@ -40,6 +53,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Home</h1>
+
+       
         <Paper>
           <Table aria-label="simple table">
             <TableHead>
