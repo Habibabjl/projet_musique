5cbefd9b4a525876d442dc18dc8de93a9741f775
BOUAJLA <bouajla.h@hotmail.fr>
2020-01-20 09:22:13 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,16 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import Search from "../components/SearchContainer"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    <h1>Home</h1>
     <div>
       <Search />
     </div>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
   </Layout>
 )
 
