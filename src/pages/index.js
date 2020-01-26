35c19f9bdae1ffaaaec3670c4efdbcea6670cd06
BOUAJLA <bouajla.h@hotmail.fr>
2020-01-26 21:02:54 +0100

    name of file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import Search from "../components/SearchContainer"
+import Search from "../components/searchcontainer"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
